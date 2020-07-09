68fec25308af3ac8721df7bf9b52111238bd704d
Michael J. Radwin <michael@radwin.org>
2020-07-09 11:19:05 -0700

    Update rollup config for more sophsiticate Node vs. brower transpilation

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,47 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
-import pkg from './package.json';
 import json from '@rollup/plugin-json';
+import pkg from './package.json';
 import {terser} from 'rollup-plugin-terser';
 
 export default [
   {
     input: 'src/index.js',
+    output: {file: pkg.main, format: 'cjs', name: pkg.name},
+    plugins: [
+      json({compact: true}),
+      babel({
+        babelHelpers: 'bundled',
+        presets: [
+          ['@babel/env', {
+            modules: false,
+            targets: {
+              node: '10.21.0',
+            },
+          }],
+        ],
+        exclude: ['node_modules/**'],
+      }),
+      resolve(),
+      commonjs(),
+    ],
+    external: ['@hebcal/core'],
+  },
+  {
+    input: 'src/index.js',
     output: [
-      {file: pkg.main, format: 'cjs', name: pkg.name},
       {
-        file: pkg.browser,
+        file: 'dist/bundle.js',
+        format: 'umd',
+        name: 'hebcal__leyning',
+        globals: {
+          '@hebcal/core': 'hebcal__core',
+        },
+        indent: false,
+      },
+      {
+        file: 'dist/bundle.min.js',
         format: 'umd',
         name: 'hebcal__leyning',
         globals: {
@@ -24,6 +54,20 @@ export default [
       json({compact: true}),
       babel({
         babelHelpers: 'bundled',
+        presets: [
+          ['@babel/env', {
+            modules: false,
+            debug: true,
+            targets: {
+              edge: '17',
+              firefox: '60',
+              chrome: '67',
+              safari: '11.1',
+            },
+            useBuiltIns: 'usage',
+            corejs: 3,
+          }],
+        ],
         exclude: ['node_modules/**'],
       }),
       resolve(),
