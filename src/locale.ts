b2b40235582d3750184b1beb12688ccdd0cf4f63
Michael J. Radwin <michael@radwin.org>
2025-10-17 22:28:28 +0000

    Add Ashkenazic translit per https://github.com/hebcal/hebcal-es6/issues/646

--- src/locale.ts
+++ src/locale.ts
@@ -1,8 +1,11 @@
 import {Locale} from '@hebcal/core/dist/esm/locale';
 import {LocaleData, StringArrayMap} from '@hebcal/hdate';
 import poHe from './he.po';
+import poAshkenazi from './ashkenazi.po';
 
 export {Locale};
+
+Locale.addTranslations('ashkenazi', poAshkenazi);
 
 Locale.addTranslations('he', poHe);
 Locale.addTranslations('h', poHe);
